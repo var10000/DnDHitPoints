fc7f6062846961f0570737e67f67e6bb2601e707
Daria Obrezkova <dariahighfly@gmail.com>
2019-08-10 22:29:16 +0300

    [add] start page with title and button

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <f7-app id="app">
+  <div class="room">
+    <header class="header">
+      <div class="header__text">
+        <p class="header__text-title"> D&D craft page</p>
+        <p class="header__text-title_description"> The  smartest app to organize your fight </p>
+      </div>
+      <div>
+        <img
+          v-on:click="isCreateCharacter = true"
+          class="header__image"
+          src="../assets/plus.png"/>
+      </div>
+    </header>
+
+    <create-character-popup
+            :opened="isCreateCharacter"
+            @popup:completed="saveCharacter"
+            @popup:closed="isCreateCharacter = false"/>
   </div>
+  </f7-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  import CreateCharacterPopup from "./components/CreateCharacterPopup"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CreateCharacterPopup,
+  },
+  data() {
+    return {
+      isCreateCharacter: false,
+    }
+  },
+  methods: {
+    saveCharacter() {
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Cinzel:700&display=swap');
+  .room {
+    background-image: url("../assets/background-dragon.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    text-align: center;
+    width: 100%;
+    min-height: 1080px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    color: white;
+  }
+  .header__text-title {
+    font-family: 'Cinzel', serif;
+    font-weight: lighter;
+    letter-spacing: 2px;
+    font-size: 40px;
+    text-align: center;
+  }
+  .header__image {
+    color: white;
+    width: 40px;
+  }
 </style>
