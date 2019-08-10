721e3a0ce93fccdb98bbdd1d25893d147e51d3ae
Filitov Mikhail <filitovme@gmail.com>
2019-08-11 01:49:41 +0300

    application builds

--- back/internal/service/bootstrap.go
+++ back/internal/service/bootstrap.go
@@ -1,12 +1,10 @@
 package service
 
 import (
-	"github.com/var10000/DnDHitPoints/back/internal/service/db"
 	"github.com/var10000/DnDHitPoints/back/internal/service/db/sqlite"
 )
 
 // InitServices returns inited services
-func InitServices() db.UserRepository {
-	ur := sqlite.CreateUserRepository()
-	return ur
+func InitServices() {
+	sqlite.CreateRepositories()
 }
