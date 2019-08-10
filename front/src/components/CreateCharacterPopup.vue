acc442cb3fdb4b546547312d31442bb2f8e47a2a
Daria Obrezkova <dariahighfly@gmail.com>
2019-08-10 22:29:16 +0300

    [add] start page with title and button

---
+++ front/src/components/CreateCharacterPopup.vue
@@ -0,0 +1,26 @@
+<template>
+    <f7-popup
+            :id="'createItemPopup'"
+            :opened="opened"
+            @popup:closed="clear">
+        <f7-page>
+            <p>PIKAKAKAKSJSJ</p>
+        </f7-page>
+    </f7-popup>
+</template>
+
+<script>
+    export default {
+        name: "CreateCharacterPopup",
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            clear() {
+
+            }
+        }
+    }
+</script>
