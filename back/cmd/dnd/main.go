3056e244e23e11f3f82dda9fa743c586ae7bd5c8
Filitov Mikhail <filitovme@gmail.com>
2019-08-11 03:12:50 +0300

    added applicetion start

--- back/cmd/dnd/main.go
+++ back/cmd/dnd/main.go
@@ -4,10 +4,9 @@ import "github.com/var10000/DnDHitPoints/back/internal/service"
 
 func main() {
 	startServices()
-
 }
 
 func startServices() {
-	service.InitServices()
-
+	app := service.InitServices()
+	app.Start()
 }
