ba9713bb89988ec2640500a5d61cf1f4c00f1063
Filitov Mikhail <filitovme@gmail.com>
2019-09-12 12:01:47 +0300

    Revert "Merge pull request #3 from var10000/delete-unused-files"
    This reverts commit 80fadbd51b815eae5f315e6233a869380fed17af, reversing
    changes made to 4abd5242732a398f82ceae038831076e04027420.

--- front2/src/store/index.js
+++ front2/src/store/index.js
@@ -4,9 +4,34 @@ import createLogger from 'vuex/dist/logger';
 
 import characters from './modules/characters';
 
+import VuexPersist from 'vuex-persist';
+
 const debug = process.env.NODE_ENV !== 'production';
 
+// const vuexPersist = new VuexPersist({
+//     key: 'my-app',
+//     storage: sessionStorage // can be localStorage
+// });
+
 Vue.use(Vuex);
+
+// const initialState = () => ({
+//     ...characters.state,
+// });
+//
+// const state = initialState();
+//
+// const getters = {
+//     ...characters.getters,
+// };
+//
+// const actions = {
+//     ...characters.actions,
+// };
+//
+// const mutations = {
+//     ...characters.mutations,
+// };
 
 const plugins = [];
 
@@ -14,6 +39,9 @@ try {
     plugins.push(
         createLogger()
     );
+    // plugins.push(
+    //     vuexPersist.plugin
+    // );
 } catch (e) {
     console.error(e);
 }
